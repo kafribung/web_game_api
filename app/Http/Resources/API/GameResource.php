c37551eaefd7e8e553644e768a918c7f84314e98
kafribung <60900117001@uin-alauddin.ac.id>
2021-02-01 22:10:05 +0800

    game resource add id

--- app/Http/Resources/API/GameResource.php
+++ app/Http/Resources/API/GameResource.php
@@ -9,6 +9,7 @@ class GameResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id'        => $this->id,
             'name'        => $this->name,
             'slug'        => $this->slug,
             'description' => $this->description,
