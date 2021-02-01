aa1ea4806ca57692e4c46df806f2a6ab3e296c9e
kafribung <60900117001@uin-alauddin.ac.id>
2021-02-01 10:24:01 +0800

    bug

--- app/Http/Controllers/Backend/QrcodeController.php
+++ app/Http/Controllers/Backend/QrcodeController.php
@@ -3,17 +3,10 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class QrcodeController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function index($id)
     {
         QrCode::size(200)->generate($id, public_path('qr-codes/' . $id . '.svg'));
